365375e7118b8fc2a1b6b10f09520828108db77d
EmmanuelMess <emmanuelbendavid@gmail.com>
2020-12-09 20:05:50 -0300

    Add code thing

--- formControl.js
+++ formControl.js
@@ -1,5 +1,6 @@
 function beautify() {
     let json = JSON.parse(document.getElementById("jsonTextArea").value);
     let niceJson = JSON.stringify(json, null, 2);
+    niceJson = "```\n" + niceJson + "\n```"
     document.getElementById("resultText").innerHTML = niceJson;
 }
