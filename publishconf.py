3cc4e6f0dfbd0b3a26f8d4ec0cbf2a2ba8838f92
EmmanuelMess <emmanuelbendavid@gmail.com>
2020-12-09 18:57:19 -0300

    Relative urls

--- publishconf.py
+++ publishconf.py
@@ -10,8 +10,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://teamamaze.github.io/ACRA-to-issue'
-RELATIVE_URLS = False
+RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
