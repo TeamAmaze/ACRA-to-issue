2be1aa56de72d54b4304b78d7cd7c7061410f4f2
EmmanuelMess <emmanuelbendavid@gmail.com>
2020-12-09 18:49:42 -0300

    Rename output to docs and change base url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://acra-mail-to-issue.github.io'
+SITEURL = 'https://teamamaze.github.io/ACRA-to-issue'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
