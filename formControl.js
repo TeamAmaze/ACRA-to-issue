99a694a99c62b3f2dafd2cfbe546f8e1e2ef2d72
EmmanuelMess <emmanuelbendavid@gmail.com>
2020-12-09 22:25:51 -0300

    Nicer issue creation

--- formControl.js
+++ formControl.js
@@ -1,6 +1,25 @@
 function beautify() {
-    let json = JSON.parse(document.getElementById("jsonTextArea").value);
-    let niceJson = JSON.stringify(json, null, 2);
-    niceJson = "```\n" + niceJson + "\n```"
-    document.getElementById("resultText").innerHTML = niceJson;
+    const raw = document.getElementById("jsonTextArea").value;
+    const json = JSON.parse(raw);
+    let output = "";
+    const rawExceptions = json["exceptions"];
+    const exceptions = rawExceptions.substring(1, json["exceptions"].length-1).split(",");
+
+    output += "**Smartphone (please complete the following information):**\n";
+    output += "- Device: " +  json["device"] + "\n";
+    output += "- OS: " +  json["os"] + "\n";
+    output += "- Rooted: Maybe\n";
+    output += "- Version: " +  json["version"] + "\n";
+
+    output += "### Stack traces:\n";
+
+    for(const exception of exceptions) {
+        output += "**Trace:**\n";
+        output +=  "```java\n" +  exception +  "```\n";
+    }
+
+    output +=  "<details><summary><b>Raw mail</b></summary><p>\n";
+    output += "```json\n" +  JSON.stringify(json, undefined, 2) + "\n```\n";
+
+    document.getElementById("resultText").innerHTML = output;
 }
